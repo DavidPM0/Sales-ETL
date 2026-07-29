926dc710a9dc0aa340566c1ea14cf562704b2874
David Padilla <davidpadilla30oc@gmail.com>
2026-07-29 10:13:01 -0500

    Add iniciar_servidor.bat script

---
+++ iniciar_servidor.bat
@@ -0,0 +1,9 @@
+@echo off
+title Servidor Web Sales ETL - http://localhost:8000
+cls
+echo ========================================================
+echo   Iniciando Servidor Web Sales ETL en http://localhost:8000
+echo ========================================================
+echo.
+npx -y http-server -p 8000
+pause
